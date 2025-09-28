fbad75551976d8e3675fcef97f4326a8b1878415
Gyashik <leninleonard7@gmail.com>
2025-09-28 19:00:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(express.json());
 // Маршрут для Webhook
 app.post('/webhook', (req, res) => {
     console.log('Received webhook:', req.body);
-    // Тут можно обработать событие и сохранить его в базу данных или выполнить другие операции
-    res.sendStatus(200); // Важно послать ответ 200, чтобы ЮMoney считала уведомление доставленным
+    // Обрабатываем событие и сохраняем в базу данных или делаем другие операции
+    res.sendStatus(200); // Важно ответить статусом 200, чтобы ЮMoney знала, что уведомление получено
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
